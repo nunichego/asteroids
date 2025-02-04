7eb0911bb7de5b8c256259eca71162794403c98e
nunichego <dmitrypuzatov@gmail.com>
2025-02-04 09:29:49 +0100

    0.13A fps updated to 60

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,6 +20,8 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
